a5a89bbbc10c04099d24cca30ed2cdbcae5d1a00
Michael Yev <michaelyevt@gmail.com>
2023-10-15 21:34:03 -0700

    added models created route for requests

---
+++ src/app/api/posts/route.js
@@ -0,0 +1,15 @@
+import Post from "@/model/Post";
+import connect from "@/app/utils/db";
+import { NextResponse } from "next/server";
+
+export const GET = async (request) => {
+    try {
+        //await connect()
+        //const posts = await Post.find()
+        return new NextResponse('Error in wwwwww', {status: 200})
+
+    }
+    catch (error) {
+        return new NextResponse('Error in response', {status: 500})
+    }
+}
