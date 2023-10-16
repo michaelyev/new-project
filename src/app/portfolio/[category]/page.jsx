963c47d64620eb11653d5b6297fe390ec5b3f864
Michael Yev <michaelyevt@gmail.com>
2023-10-15 21:06:06 -0700

    added mongo

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -17,6 +17,7 @@ const getData = (cat) =>{
 const Category = ({params}) => {
   // console.log(params);
   const data = getData(params.category)
+  console.log(params.category)
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
